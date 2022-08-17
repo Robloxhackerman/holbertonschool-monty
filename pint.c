4c22bca4ece2346e4d876ee6201b2f7855a89c94
Blas <blashernandez98@gmail.com>
2022-08-17 08:43:23 -0700

    Pint

---
+++ pint.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * pint - Function that frees a stack of ints.
+ * @head: Adress of head pointer.
+ * @line_number: Line number.
+ */
+
+void pint(stack_t **head, __attribute__((unused)) unsigned int line_number)
+{
+	if (head && *head)
+		printf("%d\n", (*head)->n);
+}
